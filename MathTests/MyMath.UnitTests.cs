bdfcd6c833c80d40f08c799b4b776b916b1b750a
Rudy Silva <rudysilva22@gmail.com>
2022-10-27 15:50:22 +1100

    Add MyMath project.

---
+++ MathTests/MyMath.UnitTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+using System;
+
+namespace MathTests
+{
+    [TestClass]
+    public class MyMathUnitTests
+    {
+        [TestMethod]
+        public void BasicRooterTest()
+        {
+            //Create a instance to test
+            Rooter rooter = new Rooter();
+
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+
+            double actualResult = rooter.SquareRoot(input);
+
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+
+        }
+
+        [TestMethod]
+        public void RooterValueRange()
+        {
+            Rooter rooter = new Rooter();
+
+            //Try a range of values
+            for (double expected = 1e-8; expected  < 1e+8; expected *= 3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterTestNegativeInput()
+        {
+            Rooter rooter = new Rooter();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => rooter.SquareRoot(-1));
+        }
+    }
+}
